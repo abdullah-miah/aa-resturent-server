2e04e0fb5cdc35f756008f192bb81ba726816dd8
unknown <mdabdullaha708@gmail.com>
2023-10-22 20:16:50 +0600

    jwt implement

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT =(req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ulqow.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -35,7 +42,8 @@ async function run() {
     
     app.post('/jwt', (req,res)=>{
       const user = req.body;
-      
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({token})
     })
 
     // users related apis

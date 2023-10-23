11ebbbcc09055d236f884d7fe1b23a51022b475b
unknown <mdabdullaha708@gmail.com>
2023-10-23 23:47:03 +0600

    admin verify seystem and token verify

--- index.js
+++ index.js
@@ -59,12 +59,23 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res.send({token})
     })
-       /***
+
+    // Warning : use verifyjwt before using verifyAdmin
+    const verifyAdmin =async(req,res,next)=>{
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        if(user.role !== 'admin'){
+           return res.status(403).send({error: true, message: 'forbidden message'})
+        };
+          next();
+    }
+       /***0. do not show secure links to those who should not see the links
         * 1. use jwt token : verifyJWT
         * 
         * * */
     // users related apis
-    app.get('/users', async(req,res)=>{
+    app.get('/users', verifyJWT, verifyAdmin, async(req,res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -79,6 +90,25 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
+      
+    // security layer : verifyJwt
+    // email same 
+    // check admin 
+
+    app.get('/users/admin/:email',verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+
+        if(req.decoded.email !== email){
+          res.send({admin: false})
+        }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'};
+      res.send(result)
+    })
+
+
   /// make admin
     app.patch('/users/admin/:id', async(req,res)=>{
       const id = req.params.id;
